d24189b6d6be712f876612df4d5ce567f9e07c48
Jun <gene4137@gmail.com>
2020-02-15 02:10:10 +0800

    基礎首頁繪製，設定全域 Sass 引用

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,101 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <header>
+      <div class="banner">
+        <p>Great Dinner Everyday</p>
+      </div>
+
+      <div class="menu-bar">
+        <div class="title">每日精選</div>
+        <div class="title">廚師推薦</div>
+        <div class="title">特價推廣</div>
+      </div>
+    </header>
+
+    <main>
+      <FoodCard />
+    </main>
+
+    <footer>
+      <span>© 2020 一 Jun</span>
+    </footer>
+
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
   </div>
 </template>
 
+<script>
+import FoodCard from '@/components/FoodCard.vue';
+
+export default {
+  components: {
+    FoodCard,
+  },
+};
+</script>
+
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
+  .banner {
+    @include size(100%, 20rem);
+    @include flex-align-justify(flex-end, center);
+    @include bg-img('./assets/images/banner.jpg', cover, center);
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    p {
+      color: #fff;
+      font-size: 3rem;
+      margin-bottom: 3rem;
+      filter: drop-shadow(darkgreen 2px 2px 1px);
     }
   }
+
+  .menu-bar {
+    @include size(100%, 3rem);
+    display: flex;
+    background-color: #000;
+
+    .title {
+      @include size((100% / 3), 100%);
+      @include flex-align-justify(center, center);
+      cursor: pointer;
+      color: #fff;
+      font-size: 1.25rem;
+    }
+  }
+
+  footer {
+    @include size(100%, 3rem);
+    @include flex-align-justify(center, center);
+    color: #fff;
+    background: #212121;
+  }
 }
+
+
+// #nav {
+//   padding: 30px;
+
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
---
+++ src/components/FoodCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="food-card">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.food-card {
+  @include size(100%, calc(100vh - 26rem));
+}
+</style>
